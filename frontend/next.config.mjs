53fe656b534d5854286cc6872effca7f1b35cf52
Ben Pick <bendavidpick@gmail.com>
2024-10-07 19:04:36 +1100

    Fix cors

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,20 @@ const nextConfig = {
     transpilePackages: ["geist"],
     env: {
         backend_host: process.env.BACKEND_HOST
+    },
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: process.env.BACKEND_HOST }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
     }
 
 };
